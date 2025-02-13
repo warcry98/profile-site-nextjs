72f37936444f453ca1dafdaa2caa38ae5f279395
warcry98 <wahidsf1905@gmail.com>
2025-02-14 03:07:35 +0700

    fixing

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/profile-site-nextjs",
   output: "export",
-  reactStrictMode: true,
+  reactStrictMode: true
 };
 
 export default nextConfig;
